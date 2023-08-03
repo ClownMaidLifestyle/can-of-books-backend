63c97d932c6df3da6ffb6e77e45ed336a3a6edfc
Elisa Edson <e.edson@protonmail.ch>
2023-08-03 10:45:15 +0100

    all CRUD operations added

--- server.js
+++ server.js
@@ -21,9 +21,34 @@ app.get("/", (request, response)=>{
     response.status(200).json("this server is responding");
 });
 
+
+//READ
+app.get("/books", async (request, response) =>{
+    try{
+        const book = await Book.find();
+        response.status(200).json(book);
+    }
+    catch (error){
+        response.status(404).json(error);
+    }
+});
+
+//CREATE
 app.post("/books", async (request, response)=>{
     const newBook = await Book.create(request.body)
     response.status(200).json(newBook)
+});
+
+//UPDATE
+app.put(`/books/:id`, async (request, response) =>{
+    await Book.findByIdAndUpdate(requet.params.id, request.body);
+});
+
+//DELETE
+app.delete(`/books/:id`, async (request, response) =>{
+    const id = request.params.id;
+    const deletedBook = await Book.findByIdAndDelete(id);
+    response.status(200).json(deletedBook);
 });
 
 
